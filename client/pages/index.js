d8d9afeab0891e26a9f17a41bc15fe18ac309271
dewarian <nathanbommezijn@gmail.com>
2021-11-22 22:44:19 +0100

    :sparkles: Add footer component to index

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar/Navbar'
+import { Footer } from '../components/footer/Footer'
 
 
 const arr = [{slug: '#one', title: 'one'},{slug: '#two', title: 'two'}]
@@ -28,19 +29,7 @@ export default function Home() {
           alt={'Shocked Pengu from League of Legends'}
         />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
