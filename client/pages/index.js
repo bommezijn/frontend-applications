17d7c0f418441ef7ddbf65dde1d021791138aad3
dewarian <nathanbommezijn@gmail.com>
2021-11-23 14:40:18 +0100

    :construction: Add dataprovider to application

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar/Navbar'
 import { Footer } from '../components/footer/Footer'
-
+import { DataProvider } from '../providers/DataContext'
 
 const arr = [{slug: '#one', title: 'one'},{slug: '#two', title: 'two'}]
 
 export default function Home() {
   return (
+  <DataProvider>
     <div className={styles.container}>
       <Head>
         <title>Frontend Applications</title>
@@ -48,6 +49,7 @@ export default function Home() {
       </main>
       <Footer />
     </div>
+    </DataProvider>
   )
 }
 
