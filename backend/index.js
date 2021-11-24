5c17eaedf7733f1768fee7776872c62c8dc9eece
dewarian <nathanbommezijn@gmail.com>
2021-11-24 21:50:31 +0100

    :sparkles: Add API route for popularListing

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 const app = require('express')();
+require('dotenv').config();
 const port = process.env.PORT || 3001;
+const fetch = require('cross-fetch')
 
-app.get('/', (req, res) => {
-  return res.json({
-    "message": "Get HTTP method"
-  })
+
+
+app.get('/api/popularListing', async (req, res) => {
+  const pageNumber = parseInt(req.query.page)
+
+  if (!pageNumber) {
+    return res.json({"error":"invalid query"})
+  } else {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=${process.env.API_KEY}&page=${req.query.page}`)
+      const data = await response.json()
+      return res.json(await data)
+    } catch (error) {
+      console.error(`/api/populrListing:`,error)
+    }
+  }
 })
 
 app.listen(port, () => {
