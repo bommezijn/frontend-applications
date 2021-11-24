5a2928311cc44be4d4676938c1d39378ba1fbc04
dewarian <nathanbommezijn@gmail.com>
2021-11-24 18:06:34 +0100

    🧼 Data sanitization integrated

--- client/contexts/DataContext.js
+++ client/contexts/DataContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from 'react'
 import fetchData from '../utilities/fetchData'
+import { roundNumber,numberToGender,createPath } from '../utilities/helpers';
 
 const testFunc = async () => {
   const response = await fetchData('https://api.themoviedb.org/3','/person/popular' );
@@ -7,12 +8,28 @@ const testFunc = async () => {
 }
 
 const DataContext = createContext([])
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
+
 
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState([])
   
   useEffect(() => {
-    testFunc().then(x => setData(x.results))
+    testFunc()
+      .then(apiData => apiData.results)
+      .then(apiResults => {
+        console.log(`apiResults:`,apiResults)
+        return apiResults.map(data => {
+          const ACTOR = {
+            name: data.name,
+            rating: roundNumber(data.popularity),
+            gender: numberToGender(data.gender),
+            photo: createPath(IMAGE_URL, data.profile_path)
+          }
+          return ACTOR
+        })
+      })
+      .then(santizedData => {console.log(`datacont useEff:`,santizedData); setData(santizedData)})
   }, [])
 
   return (
