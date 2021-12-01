39605b79968c80ece56eb7ce96e0beca0f51cb5c
dewarian <nathanbommezijn@gmail.com>
2021-12-01 19:22:24 +0100

    :construction: Start of dynamic code

--- client/contexts/DataContext.js
+++ client/contexts/DataContext.js
@@ -2,8 +2,8 @@ import { createContext, useState, useEffect } from 'react'
 import fetchData from '../utilities/fetchData'
 import { roundNumber,numberToGender,createPath } from '../utilities/helpers';
 
-const retrieveData = async () => {
-  const url = `https://frontend-applications-kvx2iph88-dewarian.vercel.app/api/popular?page=1`
+const retrieveData = async (pageNum = 1) => {
+  const url = `https://frontend-applications-kvx2iph88-dewarian.vercel.app/api/popular?page=${pageNum}`
   const response = await fetch(url)
   const data = await response.json()
   return data
@@ -13,10 +13,11 @@ const DataContext = createContext([])
 
 export const DataProvider = ({ children }) => {
   const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
+  const [pageCount, setPageCount] = useState(1)
   const [data, setData] = useState([])
   
   useEffect(() => {
-    retrieveData()
+    retrieveData(pageCount)
       .then(apiData => apiData.results)
       .then(apiResults => {
         return apiResults.map(data => {
@@ -39,6 +40,5 @@ export const DataProvider = ({ children }) => {
     <DataContext.Provider value={data}>{children}</DataContext.Provider>
   )
 }
-
 
 export default DataContext
