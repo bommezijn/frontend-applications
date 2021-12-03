d8cbf65e75c5699931e2f1a7d03ee5bf9db59f25
dewarian <nathanbommezijn@gmail.com>
2021-12-03 10:51:12 +0100

    :sparkles: Add currently playing to nav

--- client/next.config.js
+++ client/next.config.js
@@ -2,19 +2,27 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['c.tenor.com'],
+    domains: ['image.tmdb.org'],
   },
   async headers() {
     return [
       {
         // matching all API routes
-        source: "/api:path*",
+        source: '/api:path*',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,18 +1,19 @@
-import '../styles/globals.css'
-import Navbar from '../components/navbar/Navbar'
-import { Footer } from '../components/footer/Footer'
-const arr = [{slug: '/context', title: 'context'},{slug: '/two', title: 'two'}]
+import '../styles/globals.css';
+import Navbar from '../components/navbar/Navbar';
+import { Footer } from '../components/footer/Footer';
+const arr = [
+  { slug: '/context', title: 'context' },
+  { slug: '/currentPlaying', title: 'currently playing' },
+];
 
 function MyApp({ Component, pageProps }) {
-  return <>
-      <Navbar 
-      title={'Some'}
-      pages={arr}
-      handle={'bommezijn'}
-    />
-    <Component {...pageProps} />
-    <Footer />
-  </>
+  return (
+    <>
+      <Navbar title={'Some'} pages={arr} handle={'bommezijn'} />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
