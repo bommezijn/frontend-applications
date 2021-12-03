865a5c844bad95ce58855f48d8b34d2b50e16f3f
dewarian <nathanbommezijn@gmail.com>
2021-12-03 11:21:08 +0100

    :poop: fix module import

--- client/pages/currentPlaying.js
+++ client/pages/currentPlaying.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/Image';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import style from '../styles/Test.module.css';
 import { useState, useEffect } from 'react';
