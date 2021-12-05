c408a387b2aafdb2ba004a7ab27d0f1a5722d83e
dewarian <nathanbommezijn@gmail.com>
2021-12-05 21:28:12 +0100

    :bug: Fix CORS issue
    Cause of the issue was that the API is retrieving from preview instead of Prod

--- client/contexts/DataContext.js
+++ client/contexts/DataContext.js
@@ -1,5 +1,5 @@
-import { createContext, useState, useEffect } from 'react'
-import { roundNumber,numberToGender,createPath } from '../utilities/helpers';
+import { createContext, useState, useEffect } from 'react';
+import { roundNumber, numberToGender, createPath } from '../utilities/helpers';
 
 /**
  * @description data retrieval specifically for the context
@@ -7,19 +7,19 @@ import { roundNumber,numberToGender,createPath } from '../utilities/helpers';
  * @returns JSON Object
  */
 const retrieveData = async (pageNum = 1) => {
-  const url = `https://frontend-applications-kvx2iph88-dewarian.vercel.app/api/popular?page=${pageNum}`
-  const response = await fetch(url)
-  const data = await response.json()
-  return data
-}
+  const url = `https://frontend-applications-eight.vercel.app/api/popular?page=${pageNum}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
-const DataContext = createContext([])
+const DataContext = createContext([]);
 
 export const DataProvider = ({ children }) => {
   const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
-  const [pageCount, setPageCount] = useState(1)
-  const [data, setData] = useState([])
-  
+  const [pageCount, setPageCount] = useState(1);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     retrieveData(pageCount)
       .then(apiData => apiData.results)
@@ -29,20 +29,18 @@ export const DataProvider = ({ children }) => {
             name: data.name,
             rating: roundNumber(data.popularity),
             gender: numberToGender(data.gender),
-            photo: createPath(IMAGE_URL, data.profile_path)
-          }
-          return ACTOR
-        })
+            photo: createPath(IMAGE_URL, data.profile_path),
+          };
+          return ACTOR;
+        });
       })
       .then(santizedData => {
-        console.log(`datacont useEff:`,santizedData);
-        setData(santizedData)
-      })
-  }, [data.length])
+        console.log(`datacont useEff:`, santizedData);
+        setData(santizedData);
+      });
+  }, [data.length]);
 
-  return (
-    <DataContext.Provider value={data}>{children}</DataContext.Provider>
-  )
-}
+  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+};
 
-export default DataContext
+export default DataContext;
